4b46f5f33d2204d66764e11d1643eef668dff9ca
Hiroshi Maeda <inuhiroshi@gmail.com>
2014-12-14 20:15:20 +0900

    Add skeleton

---
+++ src/partition.cpp
@@ -0,0 +1,14 @@
+#include <fstream>
+#include <cstdio>
+#include <cstdlib>
+#include "patoh.h"
+
+using namespace std;
+int main(int argc, char *argv[])
+{
+    if (argc < 3) {
+        fprintf(stderr, "%s <hypergraph file> <number of parts>\n", argv[0]);
+        exit(1);
+    }
+    return 0;
+}
