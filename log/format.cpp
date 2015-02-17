f09352926284e3fe303b85fda507244ce5e042c3
Hiroshi Maeda <inuhiroshi@gmail.com>
2015-02-17 19:36:22 +0900

    Modify output format

--- log/format.cpp
+++ log/format.cpp
@@ -1,11 +1,24 @@
-
 #include <map>
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <algorithm>
 #include <cstdlib>
 #include <sstream>
+#include <vector>
 using namespace std;
+void PrintData (map<string, string> &data) {
+    cout <<        data["Matrix"] 
+        << "\t" << data["GFLOPS"]
+        << "\t" << data["NumberOfRows"]
+        << "\t" << data["NumberOfNonzeros"]
+        << "\t" << data["TotalCommunication"]
+        << "\t" << data["TotalComputation"]
+        << "\t" << data["Packing"]
+        << "\t" << data["InternalComputation"]
+        << "\t" << data["ExternalComputation"]
+        << endl;
+}
 int main (int argc, char *argv[]) {
     if (argc != 2) {
         printf("Usage: %s <log file>\n", argv[0]);
@@ -15,15 +28,25 @@ int main (int argc, char *argv[]) {
     string line;
     string matrix;
     string performance;
-    map<string, string> data;
+    vector<map<string, string> > data;
+    int p = 0;
+    map<string, string> tmp;
     while (getline(ifs, line)) {
-        if (line == "----------------------------------------") {
-            cout << data["Matrix"] << " " << data["GFLOPS"] << " " << data["NumberOfRows"] << " " << data["NumberOfNonzeros"]
-            continue;
+        if (line == "++++++++++++++++++++++++++++++++++++++++") {
+            tmp.clear();
         }
-        stringstream ss(line);
-        string key, val;
-        ss >> key >> val;
-        data[key] = val;
+        else if (line == "----------------------------------------") {
+            data.push_back(tmp);
+            continue;
+        } else {
+            stringstream ss(line);
+            string key, val;
+            ss >> key >> val;
+            tmp[key] = val;
+        }
+    }
+    sort(data.begin(), data.end(), [&](map<string, string> a, map<string, string> b) { return atoi(a["NumberOfNonzeros"].c_str()) < atoi(b["NumberOfNonzeros"].c_str()); });
+    for (auto it : data) {
+        PrintData(it);
     }
 }
