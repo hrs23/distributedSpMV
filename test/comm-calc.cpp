10731cb013515ed7b5ae053d59ed8e17523b2398
Hiroshi Maeda <inuhiroshi@gmail.com>
2015-03-06 12:11:30 +0900

    Create comm-calc.cpp for test

--- test/comm-calc.cpp
+++ test/comm-calc.cpp
@@ -3,29 +3,44 @@
 #include <mpi.h>
 using namespace std;
 int rank, size;
-const int dataSize = 100;
+const int dataSize = 1;
+const int nIteration = 1;
 
 
-void comm (dobule *sendBuffer, double *recvBuffer) {
-    MPI_Requset *sendRequest = new MPI_Request[size];
-    MPI_Requset *recvRequest = new MPI_Request[size];
+void comm (double *sendBuffer, double *recvBuffer) {
+    MPI_Request *sendRequests = new MPI_Request[size];
+    MPI_Request *recvRequests = new MPI_Request[size];
+
+
     for (int i = 0; i < size; i++) {
-        MPI_Isend(sendBuffer, dataSize, MPI_DOUBLE, i, 112233445, MPI_COMM_WORLD, sendRequest[i]);
+        MPI_Isend(sendBuffer, dataSize, MPI_DOUBLE, i, 112233445, MPI_COMM_WORLD, &sendRequests[i]);
     }
     for (int i = 0; i < size; i++) {
-        MPI_IRecv(recvBuffer + i * dataSize, dataSize, MPI_DOUBLE, i, 112233445, MPI_COMM_WORLD, recvRequest[i]);
+        MPI_Irecv(recvBuffer + i * dataSize, dataSize, MPI_DOUBLE, i, 112233445, MPI_COMM_WORLD, &recvRequests[i]);
     }
     MPI_Status *sendStatuses = new MPI_Status[size];
     MPI_Status *recvStatuses = new MPI_Status[size];
-    MPI_Waitall(size, recvReuests, recvStatuses);
-    MPI_Waitall(size, sendReuests, sendStatuses);
+    MPI_Waitall(size, recvRequests, recvStatuses);
+    MPI_Waitall(size, sendRequests, sendStatuses);
 }
 void calc (double *in, double *out) {
     for (int i = 0; i < dataSize * size; i++) {
-        for (int j = 0; j < dataSize * size; j++) {
-            out[i] = in[i] * in[j] + in[j];
-        }
+        out[i] = in[i] * in[i] + in[i] + in[i] * in[i];
     }
+}
+
+
+double GetSynchronizedTime () {
+    MPI_Barrier(MPI_COMM_WORLD);
+    double t = MPI_Wtime();
+    MPI_Bcast(&t, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    return t;
+}
+
+double GetBarrieredTime () {
+    MPI_Barrier(MPI_COMM_WORLD);
+    double t = MPI_Wtime();
+    return t;
 }
 int main (int argc, char **argv) {
     MPI_Init(&argc, &argv);
@@ -34,10 +49,73 @@ int main (int argc, char **argv) {
     double sendBuffer[dataSize];
     fill(sendBuffer, sendBuffer + dataSize, rank);
     double recvBuffer[dataSize * size];
-    comm(sendBuffer, recvBuffer);
-    calc(recvBuffer, result);
-    for (int i = 0; i < dataSize * size; i++) {
-        if (rank == 0) printf("%d\t%.1lf\n", i, result[i]);
+    double result[dataSize * size];
+    double commTime, calcTime, totalTime;
+    // Comm
+    {
+        for (int k = 0; k < nIteration; k++) {
+            double begin = GetSynchronizedTime();
+            int nLoop = 1;
+            while (GetSynchronizedTime() - begin < 1.0) {
+                for (int i = 0; i < nLoop; i++) {
+                    comm(sendBuffer, recvBuffer);
+                }
+                nLoop *= 2;
+            }
+            double elapsedTime = -GetBarrieredTime();
+            for (int i = 0; i < nLoop; i++) {
+                comm(sendBuffer, recvBuffer);
+            }
+            elapsedTime += GetBarrieredTime();
+            if (!k) commTime = elapsedTime / nLoop;
+            commTime = min(commTime, elapsedTime / nLoop);
+        }
     }
+    // Calc
+    {
+        for (int k = 0; k < nIteration; k++) {
+            double begin = GetSynchronizedTime();
+            int nLoop = 1;
+            while (GetSynchronizedTime() - begin < 1.0) {
+                for (int i = 0; i < nLoop; i++) {
+                    calc(recvBuffer, result);
+                }
+                nLoop *= 2;
+            }
+            double elapsedTime = -GetBarrieredTime();
+            for (int i = 0; i < nLoop; i++) {
+                calc(recvBuffer, result);
+            }
+            elapsedTime += GetBarrieredTime();
+            if (!k) calcTime = elapsedTime / nLoop;
+            calcTime = min(calcTime, elapsedTime / nLoop);
+        }
+    }
+    // Total
+    {
+        for (int k = 0; k < nIteration; k++) {
+            double begin = GetSynchronizedTime();
+            int nLoop = 1;
+            while (GetSynchronizedTime() - begin < 1.0) {
+                for (int i = 0; i < nLoop; i++) {
+                    comm(sendBuffer, recvBuffer);
+                    calc(recvBuffer, result);
+                }
+                nLoop *= 2;
+            }
+            double elapsedTime = -GetBarrieredTime();
+            for (int i = 0; i < nLoop; i++) {
+                comm(sendBuffer, recvBuffer);
+                calc(recvBuffer, result);
+            }
+            elapsedTime += GetBarrieredTime();
+            if (!k) totalTime = elapsedTime / nLoop;
+            totalTime = min(totalTime, elapsedTime / nLoop);
+        }
+    }
+    printf("%.5lf\t%.5lf\t%.5lf\n", commTime, calcTime, totalTime);
+    /*for (int i = 0; i < dataSize * size; i++) {
+        if (rank == 0) printf("%d\t%.1lf\n", i, result[i]);
+    }*/
     MPI_Finalize();
 }
